f00a2609973354f2bef372564e4ce48a1ab1fb41
Type-app <32158325+Shkiper195@users.noreply.github.com>
2019-11-27 13:44:26 +0400

    Add files via upload

---
+++ precache-manifest.60c1bf84b4fa554072f7fe33454eaf55.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe26ea81f78b619b6fa1228123cee59f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "637171936515e8c8dcce",
+    "url": "/static/css/main.818c3421.chunk.css"
+  },
+  {
+    "revision": "1e6f62875ef4eb4f9938",
+    "url": "/static/js/2.2ac24111.chunk.js"
+  },
+  {
+    "revision": "637171936515e8c8dcce",
+    "url": "/static/js/main.fd0472a0.chunk.js"
+  },
+  {
+    "revision": "1baad8a9a37b534391b5",
+    "url": "/static/js/runtime-main.50791c72.js"
+  },
+  {
+    "revision": "423be0591e5ceb7eb48f30e6c0c1b75c",
+    "url": "/static/media/cinema.423be059.png"
+  },
+  {
+    "revision": "71837401e9bf10020708e4716d4585fd",
+    "url": "/static/media/css.71837401.png"
+  },
+  {
+    "revision": "4aa0100fe5fc7acd171174444374b550",
+    "url": "/static/media/instagram.4aa0100f.png"
+  },
+  {
+    "revision": "6cba050585f33560736ec076fc4fb88d",
+    "url": "/static/media/intro-bg2.6cba0505.jpg"
+  },
+  {
+    "revision": "ba58ea6ca2e802e666797f292cb51f95",
+    "url": "/static/media/java.ba58ea6c.png"
+  },
+  {
+    "revision": "65873b44e2940ed66d5a8b12e5597615",
+    "url": "/static/media/lora-regular-webfont.65873b44.svg"
+  },
+  {
+    "revision": "8541664ae124b8e5df7d1af7ff907c7b",
+    "url": "/static/media/lora-regular-webfont.8541664a.eot"
+  },
+  {
+    "revision": "970ef6b8c13b9c0b50dc343c15f1576d",
+    "url": "/static/media/lora-regular-webfont.970ef6b8.woff"
+  },
+  {
+    "revision": "cf6207b3420b4c3643429addddb78eaa",
+    "url": "/static/media/lora-regular-webfont.cf6207b3.ttf"
+  },
+  {
+    "revision": "f95416d06f3402785e186be5ac2d3d61",
+    "url": "/static/media/me.f95416d0.jpg"
+  },
+  {
+    "revision": "5deef6de488f8b577a7fc90dadac57d1",
+    "url": "/static/media/mongodb.5deef6de.png"
+  },
+  {
+    "revision": "9a96398d9ce3e79946dcf87132dedef9",
+    "url": "/static/media/nodejs.9a96398d.png"
+  },
+  {
+    "revision": "13e9632bec05847de11bebb9de5ef1cf",
+    "url": "/static/media/poppins-bold-webfont.13e9632b.woff2"
+  },
+  {
+    "revision": "47a731ab9f21cd153d181fad8dd76af6",
+    "url": "/static/media/poppins-bold-webfont.47a731ab.ttf"
+  },
+  {
+    "revision": "794eab2be996e11c11bc539525c9f11b",
+    "url": "/static/media/poppins-bold-webfont.794eab2b.svg"
+  },
+  {
+    "revision": "aa98aff4d8d942f77b6dbbbcf5a5bc46",
+    "url": "/static/media/poppins-bold-webfont.aa98aff4.eot"
+  },
+  {
+    "revision": "c7422667d745ba2ce731f4765c15bb49",
+    "url": "/static/media/poppins-bold-webfont.c7422667.woff"
+  },
+  {
+    "revision": "4ee1e1689a0d398066672356c35a3f4e",
+    "url": "/static/media/poppins-medium-webfont.4ee1e168.ttf"
+  },
+  {
+    "revision": "954f16b87b54ed7c7d526c39625c459c",
+    "url": "/static/media/poppins-medium-webfont.954f16b8.woff2"
+  },
+  {
+    "revision": "c4814d6b70494e9f7282cd9ac4b41859",
+    "url": "/static/media/poppins-medium-webfont.c4814d6b.svg"
+  },
+  {
+    "revision": "f42126601d14d9bd1eab8b63196d07cc",
+    "url": "/static/media/poppins-medium-webfont.f4212660.woff"
+  },
+  {
+    "revision": "faf3fb02dbbf32176b67986efe042c24",
+    "url": "/static/media/poppins-medium-webfont.faf3fb02.eot"
+  },
+  {
+    "revision": "3509cfd5819b8cf878a79ab52dc9a01b",
+    "url": "/static/media/poppins-regular-webfont.3509cfd5.svg"
+  },
+  {
+    "revision": "42a25c26425cbb63724bc85017b14e1b",
+    "url": "/static/media/poppins-regular-webfont.42a25c26.woff"
+  },
+  {
+    "revision": "8112c2ad6ff2535b458a2ae5fac8e644",
+    "url": "/static/media/poppins-regular-webfont.8112c2ad.eot"
+  },
+  {
+    "revision": "83398fdec6508e339feb6aa038929418",
+    "url": "/static/media/poppins-regular-webfont.83398fde.ttf"
+  },
+  {
+    "revision": "cd8ad1522c0410b8ceda219c25c59308",
+    "url": "/static/media/poppins-regular-webfont.cd8ad152.woff2"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "447ab0cb2fa963339c5e08f534064237",
+    "url": "/static/media/sass.447ab0cb.png"
+  }
+]);
